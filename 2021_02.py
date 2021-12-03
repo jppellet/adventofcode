318fb1dc47a60a280e5cf1605455af765d885a73
Jean-Philippe Pellet <jppellet@gmail.com>
2021-12-03 22:27:33 +0100

    Improvements for day 2

--- 2021_02.py
+++ 2021_02.py
@@ -2,7 +2,7 @@ from typing import Tuple
 from utils import *
 
 def parse_instr(line: str) -> Tuple[str, int]:
-    [cmd, arg_str] = line.split(" ")
+    cmd, arg_str = line.split()
     return cmd, int(arg_str)
 
 instrs = read_lines(input_for(__file__), parse_instr)
@@ -10,14 +10,14 @@ instrs = read_lines(input_for(__file__), parse_instr)
 def part1():
     hpos = 0
     depth = 0
-    for instr in instrs:
-        match instr:
-            case ("forward", x):
-                hpos += x
-            case ("down", x):
-                depth += x
-            case ("up", x):
-                depth -= x
+    for cmd, arg in instrs:
+        match cmd:
+            case "forward":
+                hpos += arg
+            case "down":
+                depth += arg
+            case "up":
+                depth -= arg
 
     print(hpos * depth)
 
@@ -25,15 +25,15 @@ def part2():
     hpos = 0
     depth = 0
     aim = 0
-    for instr in instrs:
-        match instr:
-            case ("down", x):
-                aim += x
-            case ("up", x):
-                aim -= x
-            case ("forward", x):
-                hpos += x
-                depth += aim * x
+    for cmd, arg in instrs:
+        match cmd:
+            case "down":
+                aim += arg
+            case "up":
+                aim -= arg
+            case "forward":
+                hpos += arg
+                depth += aim * arg
 
     print(hpos * depth)
 
